8a7aacc0f634f9fdd7f97336c9c67f313a29d4c8
tgrigoryan-1 <tgrigoryan@ucsd.edu>
2025-10-01 11:37:24 -0700

    Question 1 for HW1 created

---
+++ HW1/Q1a.m
@@ -0,0 +1,39 @@
+% Question 1 Part a of HW 1 for MAE 108 Fall 25
+close all, clear
+
+% Move to directory where the data is downloaded
+cd('C:\Users\tigrr\Downloads')
+
+% Load the data
+load('SD_temperature.dat')
+
+% Create vectors to hold data needed
+years = SD_temperature(:,1);
+tempNov = SD_temperature(:,12);
+
+% Create histogram figure with bin width of 1 degree
+figure(1), histogram(tempNov,'BinWidth',1)
+xlabel('Temperature in November (F)')
+ylabel('Counts')
+
+% Find sample mean
+samplemean = mean(tempNov)
+% Find standard deviation
+stdtemp = std(tempNov)
+
+% Count instances of temperature being above 65 F
+hot = 0;
+for p = 1:length(tempNov)
+    tempNov(p);
+    if tempNov(p) >= 65
+        hot = hot+1;
+    end
+end
+
+% Find probability of it being hot in November
+probHot = hot/length(tempNov)
+
+
+
+
+cd('C:\Users\tigrr\UCSD\MAE108\HW1')
---
+++ HW1/Q1b.m
@@ -0,0 +1,20 @@
+% Question 1 Part b of HW 1 for MAE 108 Fall 25
+close all, clear
+
+% Move to directory where the data is downloaded
+cd('C:\Users\tigrr\Downloads')
+
+% Load the data
+load('SD_temperature.dat')
+
+% Create vectors to hold data needed
+tempAll = SD_temperature(:,2:end);
+
+% Create histogram figure with bin width of 1 degree
+figure(1), boxplot(tempAll)
+xlabel('Months in Number Form')
+ylabel('Temperature (F)')
+months = {'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'};
+xticklabels(months)
+
+cd('C:\Users\tigrr\UCSD\MAE108\HW1')
